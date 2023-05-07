3a21652901a50762e1a7fe92b2773a79f7ac5c98
dubisdev <dubisdev@gmail.com>
2023-05-07 18:42:06 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,113 @@
-import Image from 'next/image'
+"use client"
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react';
+import ReactMarkdown from "react-markdown";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [response, setResponse] = useState<undefined | string>()
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  // get query params
+  const params = useSearchParams()
+  const apiKey = params.get("apiKey")
+  const question = params.get("question")
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  useEffect(() => {
+    console.log(question)
+    askGpt(question as string, apiKey as string).then((res) => {
+      setResponse(res)
+    })
+  }, [])
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  if (!question) return (<div>Seems like you came here by error :(</div>)
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+  if (!response) return <Loading />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  return <ReactMarkdown>{response}</ReactMarkdown>
 }
+
+const Loading = () => <div className="dot-pulse"></div>
+
+const askGpt = async (prompt: string, apiKey: string) => {
+  try {
+    const response = await queryOpenAIServer(prompt, { model: "gpt-3.5-turbo", apiKey });
+
+    if (!response.ok) return "No se pudo conectar con el servidor de OpenAI"
+
+    const stream = response.body?.getReader()
+
+    if (!stream) return "No se pudo conectar con el servidor de OpenAI"
+
+    const decoder = new TextDecoder('utf-8')
+
+    while (true) {
+      const { done, value } = await stream.read()
+
+      if (done) break
+
+      const text = decoder.decode(value)
+      const data = text.split('\n').filter(Boolean).map((line) => line.trim().replace('data: ', '').trim())
+
+      let responseText = ""
+      for (const line of data) {
+        if (line === '[DONE]') {
+          break
+        }
+
+        let content = ''
+        try {
+          const json = JSON.parse(line)
+          content = json?.choices[0]?.delta?.content ?? ''
+        } catch (e) {
+          console.error('No se pudo parsear la línea', line)
+          console.error(e)
+        }
+
+        responseText += content
+      }
+
+      return responseText
+    }
+  } catch (e: any) {
+    if (e?.name === 'AbortError') return;
+  }
+}
+
+
+type OpenAISettings = {
+  model: string;
+  apiKey: string;
+}
+
+let abortController = new AbortController();
+
+const queryOpenAIServer = async (prompt: string, { model, apiKey }: OpenAISettings) => {
+  abortController.abort();
+  abortController = new AbortController();
+
+  return fetch('https://api.openai.com/v1/chat/completions', {
+    signal: abortController.signal,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({ model, stream: true, messages: [...INITIAL_GPT_MESSAGES, { role: 'user', content: prompt }] })
+  })
+}
+
+const INITIAL_GPT_MESSAGES = [{
+  role: "system",
+  content: "Eres un asistnte virtual que ayuda a los usuarios a obtener infrmación acerca de sus entradas. Este es un proyecto de prueba, por lo que responderás de manera aleatoria a las preguntas del usuario con la información que consideres adecuada a la pregunta. Si no sabes algo, debes inventarlo."
+}, {
+  role: "system",
+  content: "Los mensajes deben estar correctamente formateados en markdown. Recuerda tu rol, respondes preguntas sobre entradas. Si el usuario te pregunta algo que no tiene que ver con entradas, debes responder 'No entiendo tu pregunta. Prueba con...' e introduces una pregunta que sí puedas responder."
+}, {
+  role: "system",
+  content: "Además de las entradas, también puedes responder preguntas sobre el evento en general. Por ejemplo, si el usuario pregunta '¿Cuándo es el evento?', puedes responder 'El evento es el 1 de enero de 2022'."
+}, {
+  role: "system",
+  content: "Utiliza respuestas rápidas y concisas. No hace falta que te extiendas demasiado. Si el usuario quiere más información, puede preguntar de nuevo."
+}, {
+  role: "system",
+  content: "Solo podrás responder una vez, así que asegúrate de que tu respuesta sea la correcta. Si no lo es, el usuario tendrá que preguntar de nuevo."
+}] as const
